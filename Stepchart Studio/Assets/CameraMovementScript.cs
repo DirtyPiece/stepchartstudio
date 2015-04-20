f1e65b2c44cd6976403f1e99a14584eec6c14d90
DirtyPiece <dirtypiece@outlook.com>
2015-04-20 07:34:41 -0700

    First commit of Stepchart Studio using Unity.

---
+++ Stepchart Studio/Assets/CameraMovementScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementScript : MonoBehaviour {
+	public GameObject objectToRotateAbout;
+	public float rotationDegreesPerSecond = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalAxis = Input.GetAxis ("Horizontal");
+		transform.RotateAround (objectToRotateAbout.transform.position, new Vector3 (0, 1, 0), -horizontalAxis * rotationDegreesPerSecond * Time.deltaTime);
+	}
+}
